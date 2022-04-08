6d1777b50bbdd6b836449e05805688037266618e
Le Gany <leganybit@protonmail.com>
2022-04-08 16:25:18 +0200

    pequeños cambios

--- bicicletasadomicilio.py
+++ bicicletasadomicilio.py
@@ -3,5 +3,4 @@ from website import create_app
 app=create_app()
 
 if __name__ == "__main__":
-	app.run(debug=True, host="127.0.0.1", port=5009
-		)
+	app.run(debug=True, host="127.0.0.1", port=5009)
